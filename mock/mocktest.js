538a4699494585064a2f333e53e1fd2106b9ba04
Alpha <104521086+Alpha-022@users.noreply.github.com>
2022-08-17 17:12:22 +0530

    Add files via upload

---
+++ mock/mocktest.js
@@ -0,0 +1,108 @@
+function check() {
+    var c = 0;
+    var q1 = document.quiz.q1.value;
+    var q2 = document.quiz.q2.value;
+    var q3 = document.quiz.q3.value;
+    var q4 = document.quiz.q4.value;
+    var q5 = document.quiz.q5.value;
+    var q6 = document.quiz.q6.value;
+    var q7 = document.quiz.q7.value;
+    var q8 = document.quiz.q8.value;
+    var q9 = document.quiz.q9.value;
+    var q10 = document.quiz.q10.value;
+    var q11 = document.quiz.q11.value;
+    var q12 = document.quiz.q12.value;
+    var q13 = document.quiz.q13.value;
+    var q14 = document.quiz.q14.value;
+    var q15 = document.quiz.q15.value;
+    var q16 = document.quiz.q16.value;
+    var q17 = document.quiz.q17.value;
+    var q18 = document.quiz.q18.value;
+    var q19 = document.quiz.q19.value;
+    var q20 = document.quiz.q20.value;
+    var q21 = document.quiz.q21.value;
+    var q22 = document.quiz.q22.value;
+    var q23 = document.quiz.q23.value;
+    var q24 = document.quiz.q24.value;
+    var q25 = document.quiz.q25.value;
+    var q26 = document.quiz.q26.value;
+    var q27 = document.quiz.q27.value;
+    var q28 = document.quiz.q28.value;
+    var q29 = document.quiz.q29.value;
+    var q30 = document.quiz.q30.value;
+    var q31 = document.quiz.q31.value;
+    var q32 = document.quiz.q32.value;
+    var q33 = document.quiz.q33.value;
+    var q34 = document.quiz.q34.value;
+    var q35 = document.quiz.q35.value;
+    var q36 = document.quiz.q36.value;
+    var q37 = document.quiz.q37.value;
+    var q38 = document.quiz.q38.value;
+    var q39 = document.quiz.q39.value;
+    var q40 = document.quiz.q40.value;
+    var q41 = document.quiz.q41.value;
+    var q42 = document.quiz.q42.value;
+    var q43 = document.quiz.q43.value;
+    var q44 = document.quiz.q44.value;
+    var q45 = document.quiz.q45.value;
+    var q46 = document.quiz.q46.value;
+    var q47 = document.quiz.q47.value;
+    var q48 = document.quiz.q48.value;
+    var q49 = document.quiz.q49.value;
+    var q50 = document.quiz.q50.value;
+   
+    if (q1 == "a") { c++; }
+    if (q2 == "b") { c++; }
+    if (q3 == "b") { c++; }
+    if (q4 == "b") { c++; }
+    if (q5 == "a") { c++; }
+    if (q6 == "d") { c++; }
+    if (q7 == "c") { c++; }
+    if (q8 == "b") { c++; }
+    if (q9 == "d") { c++; }
+    if (q10 == "c") { c++; }
+    if (q11 == "d") { c++; }
+    if (q12 == "a") { c++; }
+    if (q13 == "d") { c++; }
+    if (q14 == "d") { c++; }
+    if (q15 == "d") { c++; }
+    if (q16 == "a") { c++; }
+    if (q17 == "d") { c++; }
+    if (q18 == "a") { c++; }
+    if (q19 == "a") { c++; }
+    if (q20 == "b") { c++; }
+    if (q21 == "c") { c++; }
+    if (q22 == "a") { c++; }
+    if (q23 == "d") { c++; }
+    if (q24 == "d") { c++; }
+    if (q25 == "a") { c++; }
+    if (q26 == "b") { c++; }
+    if (q27 == "a") { c++; }
+    if (q28 == "c") { c++; }
+    if (q29 == "a") { c++; }
+    if (q30 == "b") { c++; }
+    if (q31 == "c") { c++; }
+    if (q32 == "b") { c++; }
+    if (q33 == "d") { c++; }
+    if (q34 == "d") { c++; }
+    if (q35 == "b") { c++; }
+    if (q36 == "d") { c++; }
+    if (q37 == "c") { c++; }
+    if (q38 == "b") { c++; }
+    if (q39 == "c") { c++; }
+    if (q40 == "d") { c++; }
+    if (q41 == "d") { c++; }
+    if (q42 == "d") { c++; }
+    if (q43 == "a") { c++; }
+    if (q44 == "d") { c++; }
+    if (q45 == "d") { c++; }
+    if (q46 == "b") { c++; }
+    if (q47 == "c") { c++; }
+    if (q48 == "c") { c++; }
+    if (q49 == "c") { c++; }
+    if (q50 == "b") { c++; } 
+
+    const myWindow = window.open();
+    myWindow.document.write('<h1 >', "Your score is: " + c + '</h1>');
+
+}
